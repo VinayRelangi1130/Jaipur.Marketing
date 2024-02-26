ae6f6194aed4e3d9b86b60c162ea50af1c13299f
VinayRelangi1130 <rvinay1130@gmail.com>
2024-02-26 20:02:58 +0530

    bug fixed

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let a = document.getElementById("firstpage") ;
 let b = document.getElementById("secondpage") ;
+let c = document.getElementById("btn") ;
 
-a.addEventListener("click" , () => {
+c.addEventListener("click" , () => {
     b.style.display = "block" 
     a.style.display = "none"
 } )
